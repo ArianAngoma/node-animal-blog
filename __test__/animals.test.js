5d46d22455701cc1572f5b840925f3369f06148b
unknown <arian.angoma@tecsup.edu.pe>
2021-08-14 02:17:27 -0500

    Test animals

---
+++ __test__/animals.test.js
@@ -0,0 +1,24 @@
+const request = require('supertest');
+const Server = require("../src/models/server");
+const server = new Server();
+
+let testServer;
+beforeAll(() => {
+    testServer = server.app.listen(3000);
+});
+
+afterAll((done) => {
+    testServer.close(done);
+})
+
+describe('GET /', () => {
+    it('should return the sight of all animals', async () => {
+        const response = await request(server.app).get('/');
+
+        expect(response.error).toBe(false);
+        expect(response.status).toBe(200);
+        expect(response.type).toBe('text/html');
+        expect(response.header).toHaveProperty( 'content-type', 'text/html; charset=utf-8');
+        expect(response.text).not.toBeNull();
+    })
+})
